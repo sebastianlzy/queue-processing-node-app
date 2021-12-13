33bdbce1a75593710b5c0a3d3e53bc0751c4cb4b
leesebas <leesebas@amazon.com>
2021-12-13 16:59:20 +0800

    added delete message

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-// const express = require('express')
-// const app = express()
-// const port = 3000
-// const exec = require('child_process').exec;
 
-const { SQSClient, ReceiveMessageCommand } = require("@aws-sdk/client-sqs");
+const { SQSClient, ReceiveMessageCommand, DeleteMessageCommand } = require("@aws-sdk/client-sqs");
 
 
 const sqsQueueURL = process.env.QUEUE_URL
@@ -16,11 +12,23 @@ const processMessage = () => {
     const receiveMessageCommand = new ReceiveMessageCommand({
         QueueUrl: sqsQueueURL
     })
+
+
     client.send(receiveMessageCommand).then(
         (data) => {
-            console.log("RECEIVED INFORMATION FROM QUEUE")
-            console.log(data)
-            console.log("===============================")
+
+            data.Messages.forEach((message) => {
+                console.log("RECEIVED INFORMATION FROM QUEUE")
+                console.log("BODY", message.Body)
+                console.log("ATTRIBUTES", message.Attributes)
+                console.log("===============================")
+
+                const deleteMessageCommand = new DeleteMessageCommand({
+                    QueueUrl: sqsQueueURL,
+                    ReceiptHandle: message.ReceiptHandle
+                })
+            })
+
         },
         (error) => {
             console.log("ERROR RECEIVING INFORMATION FROM QUEUE")
