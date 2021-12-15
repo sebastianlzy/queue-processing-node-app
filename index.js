39c080cd49812a3cde54d9c43a518a46f25f1c03
leesebas <leesebas@amazon.com>
2021-12-15 10:13:16 +0800

    update with sqsqueueurl

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {SQSClient, SendMessageCommand} = require("@aws-sdk/client-sqs");
+const {SQSClient, SendMessageCommand, GetQueueAttributesCommand} = require("@aws-sdk/client-sqs");
 
 
 const app = express()
@@ -9,15 +9,25 @@ const client = new SQSClient({});
 
 app.get('/', (req, res) => {
     console.log(process.env)
-    res.send('Hello World!' + process.env.QUEUE_ARN)
+    const command = new GetQueueAttributesCommand({
+        QueueUrl: sqsQueueURL
+    })
+
+    return client.send(command)
+        .then((data) => {
+            res.end(data)
+        }).catch((err) => {
+            res.status(500).end(err)
+        })
 })
 
-app.get('/send-message', (req, res, next) => {
+app.get('/send-message', (req, res) => {
 
     const noOfMessage = req.query.noofmessage ? req.query.noofmessage : 50
     const promises = []
     for (let i = 0; i< noOfMessage; i++) {
         const command = new SendMessageCommand({
+            QueueUrl: sqsQueueURL,
             MessageBody: `I am message number ${i} sent at ${moment().format('MMMM Do YYYY, h:mm:ss a')}`
         })
         promises.push(client.send(command))
