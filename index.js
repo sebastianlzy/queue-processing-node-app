348b5fd8ace93bd53ce4f6a004e1c0ee9c353780
leesebas <leesebas@amazon.com>
2021-12-15 09:23:00 +0800

    log all env

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    console.log(process.env)
+    res.send('Hello World!' + process.env.QUEUE_ARN)
 })
 
 app.listen(port, () => {
